124d0d2ab9a0a8978d841ab9b971a0c000be8c03
Nagesha S <43410978+NageshaSriram@users.noreply.github.com>
2024-01-23 17:17:08 +0530

    TinyMCE and React Inegration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import HTMLReactParser from "html-react-parser";
+
+import Prism from "prismjs";
+import "prismjs/themes/prism-okaidia.css";
+
+import "prismjs/components/prism-java";
+
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+import "prismjs/plugins/line-numbers/prism-line-numbers";
 
 function App() {
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    if (content) {
+      Prism.highlightAll();
+    }
+  }, [content]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Editor
+        apiKey="jeiltvbooyoyr3fe3xt511l45gkbcj9wrq1pj5hhpfrsqmua"
+        onChange={(event) => setContent(event.target.getContent())}
+        init={{
+          plugins:
+            "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount codesample code",
+          toolbar:
+            "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | codesample code | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat",
+        }}
+        initialValue="Welcome to TinyMCE!"
+      />
+      <div>
+        <h1>Displaying Editor contents</h1>
+        <hr />
+        <br />
+        <span>{HTMLReactParser(content)}</span>
+      </div>
+    </>
   );
 }
 
